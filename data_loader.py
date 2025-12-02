1d38ade0297af771e82cbca858975e0295c743b1
bsanchezv <iamsanbarb@gmail.com>
2025-12-01 19:39:38 -0500

    project_root added to call the load_and_spli_data() properly within the notebooks

--- data_loader.py
+++ data_loader.py
@@ -1,10 +1,23 @@
+import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def load_and_split_data(config):
+    """Load data from CSV files and split into training and validation sets.
+    Args:
+        config (dict): Configuration dictionary containing file paths and parameters.
+    Returns:
+        X_train (pd.DataFrame): Training features.
+        X_val (pd.DataFrame): Validation features.
+        y_train (pd.Series): Training target.
+        y_val (pd.Series): Validation target.
+    """
+    # Determine the project root directory
+    project_root = os.path.dirname((__file__))
+    train_path = os.path.join(project_root, config['data']['train_path'])
+
     # Load data
-    train_df = pd.read_csv(config['data']['train_path'])
-    test_df = pd.read_csv(config['data']['test_path'])
+    train_df = pd.read_csv(train_path)
 
     # Split features and target
     X = train_df.drop(config['modeling']['TARGET_COLUMN'], axis=1)
@@ -16,3 +29,5 @@ def load_and_split_data(config):
         test_size=config['modeling']['VALID_SIZE'], # for validation size, actually. The test size is already defined by the test set
         random_state=config['base']['RANDOM_SEED']
     )
+
+    return X_train, X_val, y_train, y_val
